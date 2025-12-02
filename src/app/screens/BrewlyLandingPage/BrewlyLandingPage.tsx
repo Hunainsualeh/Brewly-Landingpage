851126ca4d6fae9e9a8796f6a4a1f38624e9108e
Hunain Sualeh <hunainsualeh8@gmail.com>
2025-12-02 15:20:41 +0500

    Brewly landing page creation

--- src/app/screens/BrewlyLandingPage/BrewlyLandingPage.tsx
+++ src/app/screens/BrewlyLandingPage/BrewlyLandingPage.tsx
@@ -2,14 +2,17 @@
 
 import React from "react";
 import { FeaturesSection } from "../sections/FeauturSection";
-import { HeroSection } from "../sections/HeroSection";
+
 import { NavigationSection } from "../sections/NavigationSeciotn";
 
 export const BrewlyLandingPage = () => {
   return (
-    <div className="w-full min-w-0 flex flex-col bg-white overflow-x-hidden" data-model-id="1665:3317">
+    <div
+      className="w-full min-w-0 flex flex-col bg-white overflow-x-hidden"
+      data-model-id="1665:3317"
+    >
       <NavigationSection />
-    
+
       <FeaturesSection />
     </div>
   );
